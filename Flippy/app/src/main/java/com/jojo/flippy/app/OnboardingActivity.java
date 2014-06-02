a2e3b6e96719ddd76730ed92ccdbc3f85d81e8b0
Jesse <williamsaintgeorge@gmail.com>
2014-06-02 23:07:04 +0000

    Completed OnboardingActivity. Designed custom buttons.

--- Flippy/app/src/main/java/com/jojo/flippy/app/OnboardingActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/app/OnboardingActivity.java
@@ -1,16 +1,49 @@
 package com.jojo.flippy.app;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
 
-public class OnboardingActivity extends Activity {
+
+public class OnboardingActivity extends FragmentActivity {
+
+    private static final int NUMBER_OF_PAGES = 4;
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new OnboardingPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    private class OnboardingPagerAdapter extends FragmentStatePagerAdapter {
+        public OnboardingPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return OnboardingTextFragment.create(position);
+        }
+
+        @Override
+        public int getCount() {
+            return NUMBER_OF_PAGES;
+        }
     }
 }
