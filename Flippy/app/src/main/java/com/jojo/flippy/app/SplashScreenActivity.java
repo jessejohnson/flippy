4a48aa39eb8aab9efe71a995e9cf3c7f873b2a67
Jesse <williamsaintgeorge@gmail.com>
2014-06-02 17:05:10 +0000

    added timer delay to SplashActivity.onCreate.

--- Flippy/app/src/main/java/com/jojo/flippy/app/SplashScreenActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/app/SplashScreenActivity.java
@@ -1,9 +1,13 @@
 package com.jojo.flippy.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class SplashScreenActivity extends Activity {
@@ -12,6 +16,16 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        //TODO add timer for splash screen delay. Later, load relevant resources from API here
+        //TODO Load relevant resources from API here
+
+        /*onCreate variables*/
+        int DELAY = 1000;
+        
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, OnboardingActivity.class));
+            }
+        }, DELAY);
     }
 }
