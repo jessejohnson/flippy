7917027e70d430d98c7c40444fdf4b362268cb54
bright <brightahedor@gmail.com>
2014-06-06 13:30:50 +0000

    Added a function to the SelectCommunityActivity that loads the spinner during the on create call

--- Flippy/app/src/main/java/com/jojo/flippy/app/SelectCommunityActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/app/SelectCommunityActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SelectCommunityActivity extends ActionBarActivity {
-
+    private Spinner spinnerSelectCommunity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +22,24 @@ public class SelectCommunityActivity extends ActionBarActivity {
         ActionBar actionbar = getActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setSubtitle(getString(R.string.last_step));
+
+        //Identify the spinner from the layout and call the function to add items
+        spinnerSelectCommunity = (Spinner) findViewById(R.id.spinnerSelectCommunity);
+        addItemsOnCommunitySpinner();
+
+    }
+
+    // add items into spinner dynamically
+    public void addItemsOnCommunitySpinner() {
+        List<String> communityList = new ArrayList<String>();
+        //TODO run a loop to add the list of communities available
+        communityList.add("University of Ghana");
+        communityList.add("KNUST");
+        communityList.add("University of Cape coast");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, communityList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSelectCommunity.setAdapter(dataAdapter);
     }
 
     @Override
