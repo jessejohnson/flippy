52989c8dc05bec8a07105c5454e1427fe159d774
bright <brightahedor@gmail.com>
2014-06-09 20:25:42 +0000

    Moved the community activities to the core package

--- Flippy/app/src/main/java/com/jojo/flippy/app/SelectCommunityActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/app/SelectCommunityActivity.java
@@ -4,14 +4,12 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-import com.jojo.flippy.community.ChannelFeeds;
+import com.jojo.flippy.core.ChannelFeeds;
 
 import java.util.ArrayList;
 import java.util.List;
