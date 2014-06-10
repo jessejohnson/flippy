c1a4284d39b57f5a34c9d52607d776b15f884c1a
bright <brightahedor@gmail.com>
2014-06-10 00:37:41 +0000

    Added the navigation drawer, except the items of the drawer

--- Flippy/app/src/main/java/com/jojo/flippy/adapter/CustomDrawer.java
+++ Flippy/app/src/main/java/com/jojo/flippy/adapter/CustomDrawer.java
@@ -11,7 +11,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.jojo.flippy.app.R;
@@ -47,6 +49,17 @@ public class CustomDrawer extends ArrayAdapter<DrawerItem> {
                     .findViewById(R.id.drawer_itemName);
             drawerHolder.icon = (ImageView) view.findViewById(R.id.drawer_icon);
 
+            drawerHolder.accountLayout = (LinearLayout) view
+                    .findViewById(R.id.accountLayout);
+            drawerHolder.itemLayout = (LinearLayout) view
+                    .findViewById(R.id.itemLayout);
+
+            drawerHolder.user_frame = (FrameLayout)view.findViewById(R.id.user_frame);
+
+            drawerHolder.userImage = (ImageView) view.findViewById(R.id.user_pic);
+            drawerHolder.userName = (TextView)view.findViewById(R.id.text_user_name);
+
+
             view.setTag(drawerHolder);
 
         } else {
@@ -55,10 +68,20 @@ public class CustomDrawer extends ArrayAdapter<DrawerItem> {
         }
 
         DrawerItem dItem = (DrawerItem) this.drawerItemList.get(position);
-
-        drawerHolder.icon.setImageDrawable(view.getResources().getDrawable(
-                dItem.getImgResID()));
-        drawerHolder.ItemName.setText(dItem.getItemName());
+        if(position==0){
+            drawerHolder.itemLayout.setVisibility(LinearLayout.INVISIBLE);
+            drawerHolder.accountLayout.setVisibility(LinearLayout.VISIBLE);
+            drawerHolder.userName.setText("Bright Ahedor");
+            drawerHolder.userImage.setImageResource(R.drawable.test);
+        }else {
+            drawerHolder.user_frame.setVisibility(View.GONE);
+            drawerHolder.userImage.setVisibility(View.GONE);
+            drawerHolder.accountLayout.setVisibility(View.GONE);
+            drawerHolder.itemLayout.setVisibility(LinearLayout.VISIBLE);
+            drawerHolder.icon.setImageDrawable(view.getResources().getDrawable(
+                    dItem.getImgResID()));
+            drawerHolder.ItemName.setText(dItem.getItemName());
+        }
 
         return view;
     }
@@ -66,5 +89,9 @@ public class CustomDrawer extends ArrayAdapter<DrawerItem> {
     private static class DrawerItemHolder {
         TextView ItemName;
         ImageView icon;
+        ImageView userImage;
+        TextView userName;
+        LinearLayout itemLayout,accountLayout;
+        FrameLayout user_frame;
     }
 }
