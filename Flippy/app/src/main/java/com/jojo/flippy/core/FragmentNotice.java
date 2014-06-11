74e5f5d95f3bb5794300d996e23a1977a58e6a85
Jesse <williamsaintgeorge@gmail.com>
2014-06-11 11:13:16 +0000

    created Notice class, Adapter and ListView in NoticeFragment

--- Flippy/app/src/main/java/com/jojo/flippy/core/FragmentNotice.java
+++ Flippy/app/src/main/java/com/jojo/flippy/core/FragmentNotice.java
@@ -5,18 +5,26 @@ package com.jojo.flippy.core;
  */
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.jojo.flippy.app.R;
 
+import java.net.URI;
+import java.util.ArrayList;
+
 public class FragmentNotice extends Fragment {
 
-    ImageView ivIcon;
-    TextView tvItemName;
+    ListView noticeList;
+    ListAdapter listAdapter;
+    ArrayList<Notice> noticeFeed = new ArrayList<Notice>();
 
 
     public FragmentNotice() {
@@ -30,8 +38,17 @@ public class FragmentNotice extends Fragment {
         View view = inflater.inflate(R.layout.fragment_notice, container,
                 false);
 
-        ivIcon = (ImageView) view.findViewById(R.id.frag1_icon);
-        tvItemName = (TextView) view.findViewById(R.id.frag1_text);
+        //populate noticeFeed with dummy values to test app
+        noticeFeed.add(new Notice("1", "admin", "SRC2014", "Welcome Back!", "sub",
+                "We the SRC2014 welcome all freshers to school, and all continuing students" +
+                        " back to school", URI.create("")));
+        noticeFeed.add(new Notice("2", "echo", "Echo Legon", "Fresher's Party!", "sub",
+                "The semester begins with the hottest freshers at Aphro, Friday night!",
+                URI.create("")));
+
+        listAdapter = new NoticeListAdapter(this.getActivity(), noticeFeed);
+        noticeList = (ListView) view.findViewById(R.id.listViewNoticeList);
+        noticeList.setAdapter(listAdapter);
 
 
         return view;
---
+++ Flippy/app/src/main/java/com/jojo/flippy/core/Notice.java
@@ -0,0 +1,87 @@
+package com.jojo.flippy.core;
+
+import java.net.URI;
+import java.util.Date;
+
+/**
+ * Created by odette on 6/11/14.
+ */
+public class Notice {
+    String id;
+    String creatorId;
+    String channelId;
+    String title;
+    String subtitle;
+    String content;
+    URI imageUrl;
+    Date timeStamp;
+    Date reminderDate;
+    String location;
+    int Starts;
+
+    public Notice(String id, String creatorId, String channelId, String title, String subtitle, String content, URI imageUrl) {
+        this.id = id;
+        this.creatorId = creatorId;
+        this.channelId = channelId;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle(){
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle){
+        this.subtitle = subtitle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public URI getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(URI imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
