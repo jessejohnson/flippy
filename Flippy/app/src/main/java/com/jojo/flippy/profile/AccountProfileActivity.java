2cb6db269f116897733414f649629e848b1cc6e7
Jesse <williamsaintgeorge@gmail.com>
2014-06-14 12:57:18 +0000

    amended channel list display in profile.

--- Flippy/app/src/main/java/com/jojo/flippy/profile/AccountProfileActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/profile/AccountProfileActivity.java
@@ -8,18 +8,13 @@ import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.jojo.flippy.adapter.Channel;
-import com.jojo.flippy.adapter.CustomUserChannel;
+import com.jojo.flippy.adapter.ChannelAdapter;
 import com.jojo.flippy.app.R;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.List;
 
 public class AccountProfileActivity extends ActionBarActivity {
-    //Instance of the user channel
-    ListView userChannelListView;
-    //Instance of the channel item
-    List<Channel> rowItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +26,24 @@ public class AccountProfileActivity extends ActionBarActivity {
         actionBar.setSubtitle(R.string.user_tap_to_edit);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-       //Loading the list with a dummy data
-        rowItems = new ArrayList<Channel>();
-        Channel item = new Channel(URI.create(""), "GESA KNUST", "200 members","active");
-        Channel item1 = new Channel(URI.create(""), "SRC Legon, 2015", "4000 members","admin");
+        //Loading the list with a dummy data
+        ArrayList<Channel> rowItems = new ArrayList<Channel>();
+        Channel item = new Channel(URI.create("http://images-mediawiki-sites.thefullwiki.org/02/1/0/0/73473104099591446.jpg"),
+                "GESA KNUST", "200 members","active");
+        Channel item1 = new Channel(URI.create("http://www.ugsrc.com/wp-content/uploads/2013/08/6.jpg"), "SRC Legon, 2015", "4000 members","admin");
+        Channel item2 = new Channel(URI.create("http://images-mediawiki-sites.thefullwiki.org/02/1/0/0/73473104099591446.jpg"),
+                "PENSA KNUST", "200 members","active");
+        Channel item3 = new Channel(URI.create("http://images-mediawiki-sites.thefullwiki.org/02/1/0/0/73473104099591446.jpg"),
+                "QUEENS HALL", "200 members","active");
         rowItems.add(item);
         rowItems.add(item1);
+        rowItems.add(item2);
+        rowItems.add(item3);
 
-
-        userChannelListView = (ListView) findViewById(R.id.listViewUserChannels);
-        CustomUserChannel adapter = new CustomUserChannel(this,
-                R.layout.user_channel_list_item, rowItems);
-        userChannelListView.setAdapter(adapter);
-
-
-
-
+        ListView profileChannelList = (ListView) findViewById(R.id.profileChannelListView);
+        ChannelAdapter channelAdapter = new ChannelAdapter(AccountProfileActivity.this,
+                R.layout.channel_listview, rowItems, true);
+        profileChannelList.setAdapter(channelAdapter);
     }
 
 
