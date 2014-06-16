65b798f3d7dd9b69fd1b36296b5c4e9cba338e3f
bright <brightahedor@gmail.com>
2014-06-16 00:35:58 +0000

    Added an image preview activity, and also the member detal activity

--- Flippy/app/src/main/java/com/jojo/flippy/profile/MemberDetailActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/profile/MemberDetailActivity.java
@@ -1,13 +1,19 @@
 package com.jojo.flippy.profile;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.jojo.flippy.app.R;
 
 public class MemberDetailActivity extends ActionBarActivity {
+    private ImageView imageViewMemberAnotherUserProfilePic;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,38 @@ public class MemberDetailActivity extends ActionBarActivity {
         actionBar.setTitle("Bright Profile");
         actionBar.setSubtitle(R.string.user_tap_to_edit);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        intent = getIntent();
+        intent.setClass(MemberDetailActivity.this,ImagePreviewActivity.class);
+
+
+        imageViewMemberAnotherUserProfilePic = (ImageView)findViewById(R.id.imageViewMemberAnotherUserProfilePic);
+        imageViewMemberAnotherUserProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.member_detail, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_full_screen) {
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
