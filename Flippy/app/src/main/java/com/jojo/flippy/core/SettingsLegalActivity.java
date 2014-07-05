83aa7399b0ddbde8b193e4c8025f27c4d5bfbd14
Jesse <williamsaintgeorge@gmail.com>
2014-07-05 15:03:44 +0000

    Changes to SettingsLegalActivity

--- Flippy/app/src/main/java/com/jojo/flippy/core/SettingsLegalActivity.java
+++ Flippy/app/src/main/java/com/jojo/flippy/core/SettingsLegalActivity.java
@@ -1,9 +1,10 @@
 package com.jojo.flippy.core;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.jojo.flippy.core.R;
 
 public class SettingsLegalActivity extends ActionBarActivity {
